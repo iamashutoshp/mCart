61d68c5b43e7dca191b498cac1aeb8c0421a9a9f
iamashutoshp <ap1853177@gmail.com>
2021-10-06 04:20:06 +0530

    welcome component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ProductsComponent } from './products/products.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ProductsComponent
+    ProductsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
